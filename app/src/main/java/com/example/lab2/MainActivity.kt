f39de2c8c86730b47cab9290da414e5b9b96c3ad
Robert Ohly <ohly0001@algonquinlive.com>
2025-09-15 00:22:06 -0400

    Added custom theme

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,33 +1,83 @@
 package com.example.lab2
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.example.lab2.ui.theme.Lab2Theme
+import com.example.lab2.ui.theme.Typography
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color(0xff2c2d32),
+    secondary = Color(0xff1b4b4b),
+    tertiary = Color(0xff263437)
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Color(0xffd9d9e1),
+    secondary = Color(0xff66b2b2),
+    tertiary = Color(0xffb3c5c5)
+)
+
+@Composable
+fun Theme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+){
+    val colorScheme = when {
+        dynamicColor -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Surface(
-                color =
-                contentColor = Color.Red,
-            ){
-                Text(text = stringResource(R.string.hello_message))
+            Theme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Text(text = stringResource(R.string.hello_message))
+                }
             }
         }
     }
